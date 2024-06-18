9cf5b455c0c0b80b128b3b5ad2d9be97c67a70db
ouata <ayoub.programmierwelt@gmail.com>
2024-06-18 04:03:41 +0200

    Defining and executing functions.

---
+++ define and execute functions.py
@@ -0,0 +1,16 @@
+# Funktionen definieren und aufrufen:
+
+# Die Definition einer Funktion startet immer mit "def" und danach einen Bezeichner.
+# Diese von mir definierte Funktion kann ich jetzt permanent im Programm aufrufen.
+def greeting():
+    print("Hallo Welt.")  # Funktionskörper: Hier kommen alle Anweisungen rein, die dann von der Funktion ausgeführt
+    print("7 Kontinente")  # werden sollen, wenn die Funktion von mir später aufgerufen wird.
+    print("Meine erste Funktion.")
+
+
+# Aufrufen/Ausführen der Funktion:
+greeting()
+print("Ich bin das Hauptprogramm.")
+greeting()
+print("Ich bin immernoch das Hauptprogramm.")
+greeting()

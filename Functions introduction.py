db37607787f4d0eb64ca0b00222f1cf62ee0dd28
ouata <ayoub.programmierwelt@gmail.com>
2024-06-18 03:38:40 +0200

    Functions introduction theory.

---
+++ Functions introduction.py
@@ -0,0 +1,24 @@
+# Wenn die Programme immer länger und komplexer werden, dann kennt man sich irgendwann nicht mehr
+# wirklich aus, wenn man alle Anweisungen einfach nur der Reihe nach in sein File reinschreibt.
+# Einer der Gründe, wieso das Konzept von Funktionen entwickelt wurde.
+# Mithilfe dieser habe ich die Möglichkeit den Code im Programm besser zu strukturieren, in dem ich
+# einzelne Funktionalitäten des Programms, in jeweils unterschiedliche Funktionen auslagere.
+
+# Prinzip von Funktionen:
+# 1. Das große komplexe Problem, welches mein Programm lösen soll, in viele kleine Teilprobleme
+# aufgliedern.
+
+# 2. Für jedes enstandene Teilproblem gibt es dann eine oder mehrere Funktionen, die dieses lösen.
+# Funktionen sind also Unterprogramme, die über ihren Funktionsnamen aufgerufen werden können.
+
+# Vorteile von Funktionen:
+# - Bessere Strukturierung.
+# - Vermeidung von redundanten Code.
+# In einem Programm kommt es sehr oft vor, dass bestimmte Aufgaben immer wieder ausgeführt werden müssen.
+# Es wäre sehr unpraktisch, wenn man immer den gleichen Code wiederholt innerhalb eines Programms hinschreiben müsste.
+# Es würde das Programm nur unnötig aufblähen und hätte somit sehr viel redundanten Code.
+# Also sehr viele Stellen im Programm, an dem der komplett gleiche Code stehen würde.
+# Und wenn ich diesen redundanten Code dann abändern möchte, dann müsste man dies an zahlreichen Stellen im Programm tun
+# und das Risiko wäre sehr groß, dass man das an einer Stelle vergisst und somit Fehler entstehen.
+# Bessere Wartbarkeit.
+# Zum Beispiel: Print-Funktion: Dabei handelt es sich um ein bild-in function.
